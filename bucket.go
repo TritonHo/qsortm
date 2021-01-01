d3adb99d49036290d3c743fd0ca969ca600ea917
Triton Ho <5516113+TritonHo@users.noreply.github.com>
2021-01-01 20:36:19 +0000

    bug fix

--- bucket.go
+++ bucket.go
@@ -181,7 +181,7 @@ func qsortWithBucket(input []int) {
 	}
 	// add the first and last bucket
 	go bucketWorker(input, input[:finalizedPivotPositions[0]], finalizedPivotPositions, exchangeChannels, 0, ch1)
-	go bucketWorker(input, input[finalizedPivotPositions[pivotCount-1]+1:], finalizedPivotPositions, exchangeChannels, 0, ch1)
+	go bucketWorker(input, input[finalizedPivotPositions[pivotCount-1]+1:], finalizedPivotPositions, exchangeChannels, pivotCount, ch1)
 
 	// wait for all task done, and the qsort worker thread die peacefully
 	remainingTaskNum.Wait()
