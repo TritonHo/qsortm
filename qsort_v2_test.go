230417bee6191e7b4e873a196f4605a5f119f5c9
Triton Ho <5516113+TritonHo@users.noreply.github.com>
2021-01-24 18:03:49 +0000

    add more testcase

--- qsort_v2_test.go
+++ qsort_v2_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func getSampleInput() []int {
-	return []int{10, 1, 2, 3, 4, 11, 12, 13, 14, 15}
+	return []int{10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5}
 }
 
 func verifyIntArray(t *testing.T, expectedInput, actualInput []int) {
@@ -21,16 +21,70 @@ func TestSubTaskInternal1(t *testing.T) {
 	input := getSampleInput()
 
 	st := subtask{
-		left:     sliceRange{start: 1, end: 5},
-		right:    sliceRange{start: 5, end: 10},
+		left:     sliceRange{start: 1, end: 6},
+		right:    sliceRange{start: 6, end: 11},
 		pivotPos: 0,
 	}
 
 	result := subTaskInternal(input, st)
-	expected := []int{10, 1, 2, 3, 4, 11, 12, 13, 14, 15}
+	expected := []int{10, 5, 4, 3, 2, 1, 15, 14, 13, 12, 11}
 
 	verifyIntArray(t, expected, input)
 	if result.leftFinished != 0 || result.rightFinished != 0 {
+		t.Error("leftFinished / rightFinished not match", result.leftFinished, result.rightFinished)
+	}
+}
+
+func TestSubTaskInternal2(t *testing.T) {
+	input := getSampleInput()
+
+	st := subtask{
+		left:     sliceRange{start: 1, end: 5},
+		right:    sliceRange{start: 7, end: 11},
+		pivotPos: 0,
+	}
+
+	result := subTaskInternal(input, st)
+	expected := []int{10, 5, 4, 3, 2, 15, 1, 14, 13, 12, 11}
+
+	verifyIntArray(t, expected, input)
+	if result.leftFinished != 0 || result.rightFinished != 0 {
+		t.Error("leftFinished / rightFinished not match", result.leftFinished, result.rightFinished)
+	}
+}
+
+func TestSubTaskInternal3(t *testing.T) {
+	input := getSampleInput()
+
+	st := subtask{
+		left:     sliceRange{start: 1, end: 5},
+		right:    sliceRange{start: 8, end: 11},
+		pivotPos: 0,
+	}
+
+	result := subTaskInternal(input, st)
+	expected := []int{10, 5, 4, 3, 14, 15, 1, 2, 13, 12, 11}
+
+	verifyIntArray(t, expected, input)
+	if result.leftFinished != 1 || result.rightFinished != 0 {
+		t.Error("leftFinished / rightFinished not match", result.leftFinished, result.rightFinished)
+	}
+}
+
+func TestSubTaskInternal4(t *testing.T) {
+	input := getSampleInput()
+
+	st := subtask{
+		left:     sliceRange{start: 1, end: 4},
+		right:    sliceRange{start: 7, end: 11},
+		pivotPos: 0,
+	}
+
+	result := subTaskInternal(input, st)
+	expected := []int{10, 5, 4, 3, 14, 15, 1, 2, 13, 12, 11}
+
+	verifyIntArray(t, expected, input)
+	if result.leftFinished != 0 || result.rightFinished != 1 {
 		t.Error("leftFinished / rightFinished not match", result.leftFinished, result.rightFinished)
 	}
 }
