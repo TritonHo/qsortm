e67a90fc8d40612e0d407e3f73cac11f3b3904f3
Triton Ho <5516113+TritonHo@users.noreply.github.com>
2021-01-30 18:19:36 +0000

    add back the qsort lib

---
+++ qsort.go
@@ -0,0 +1,93 @@
+package qsortm
+
+import (
+	//	"log"
+
+	"sort"
+
+	"runtime"
+	"sync"
+)
+
+type task struct {
+	startPos, endPos int
+}
+
+func channelInverter(inputCh, outputCh chan task) {
+	taskBuffer := []task{}
+	for {
+		newTask, ok := <-inputCh
+		if !ok {
+			// the input channel has already closed
+			break
+		}
+		taskBuffer = append(taskBuffer, newTask)
+		for len(taskBuffer) > 0 {
+			select {
+			case outputCh <- taskBuffer[len(taskBuffer)-1]:
+				taskBuffer = taskBuffer[:len(taskBuffer)-1]
+			case newTask := <-inputCh:
+				taskBuffer = append(taskBuffer, newTask)
+			}
+		}
+	}
+
+	close(outputCh)
+}
+
+func qsortProdWorker(input []int, inputCh, outputCh chan task, wg, remainingTaskNum *sync.WaitGroup) {
+
+	// if the size of the task is below threshold, it will use the standard library for sorting
+	// too small threshold will cause necessary data exchange between threads and degrade performance
+	const threshold = 10000
+
+	defer wg.Done()
+
+	for t := range inputCh {
+		n := t.endPos - t.startPos
+		switch {
+		case n > threshold:
+			// FIXME: choose a better pivot choosing algorithm instead of hardcoding
+			pivotPos := t.startPos
+			finalPivotPos := qsortPartition(input, t.startPos, t.endPos, pivotPos)
+
+			// add the sub-tasks to the queue
+			remainingTaskNum.Add(2)
+			outputCh <- task{startPos: t.startPos, endPos: finalPivotPos}
+			outputCh <- task{startPos: finalPivotPos + 1, endPos: t.endPos}
+		case n >= 2:
+			// for small n between 2 to threshold, we switch back to standard library
+			sort.Ints(input[t.startPos:t.endPos])
+		}
+
+		// mark the current task is done
+		remainingTaskNum.Done()
+	}
+}
+
+func qsortProd(input []int) {
+	wg := sync.WaitGroup{}
+	remainingTaskNum := sync.WaitGroup{}
+
+	threadNum := runtime.NumCPU()
+	// ch1 link from inverter --> worker, it should be unbuffered allow FILO behaviour in coordinator
+	ch1 := make(chan task, threadNum)
+	// ch2 link from worker --> inverter, it pass the sub-task
+	ch2 := make(chan task, 100*threadNum)
+
+	wg.Add(threadNum)
+	for i := 0; i < threadNum; i++ {
+		go qsortProdWorker(input, ch1, ch2, &wg, &remainingTaskNum)
+	}
+
+	go channelInverter(ch2, ch1)
+
+	// add the input to channel
+	remainingTaskNum.Add(1)
+	ch1 <- task{startPos: 0, endPos: len(input)}
+	remainingTaskNum.Wait()
+
+	// wait for all task done, and the worker thread die peacefully
+	close(ch2)
+	wg.Wait()
+}
---
+++ qsort_test.go
@@ -0,0 +1,180 @@
+package qsortm
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+
+	"log"
+	"time"
+)
+
+func generateRandomSlice(n int) []int {
+
+	slice := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		slice[i] = rand.Int()
+	}
+	return slice
+}
+
+func isAscSorted(slice []int) bool {
+
+	for i := 1; i < len(slice); i++ {
+		if slice[i-1] > slice[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func sliceToCounters(slice []int) map[int]int {
+	counters := map[int]int{}
+	for _, item := range slice {
+		counters[item] = counters[item] + 1
+	}
+	return counters
+}
+
+func verifySliceCounters(slice []int, counters map[int]int) bool {
+	for _, item := range slice {
+		counters[item] = counters[item] - 1
+	}
+	for _, v := range counters {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func TestCountBucketSize(t *testing.T) {
+	input := generateRandomSlice(100000)
+
+	pivotPositions := getPivotPositions(input, 100)
+	pivots := countBucketSize(input, pivotPositions)
+
+	for _, v := range input {
+		for i := range pivots {
+			if v <= input[pivots[i].pos] {
+				pivots[i].count--
+				break
+			}
+		}
+	}
+	for i, p := range pivots {
+		if p.count != 0 {
+			t.Error("the count not match", i, p)
+		}
+	}
+	log.Println("TestCountBucketSize finished")
+}
+
+func TestRelocatePivots(t *testing.T) {
+	input := generateRandomSlice(10000)
+	input2 := []int{}
+	for _, value := range input {
+		input2 = append(input2, value)
+	}
+
+	pivotPositions := getPivotPositions(input, 100)
+	pivots := countBucketSize(input, pivotPositions)
+	mergedPivots := mergePivots(input, pivots, 10)
+	finalizedPivotPositions := relocatePivots(input, mergedPivots)
+
+	// get the value of the pivots
+	values := []int{}
+	for _, pos := range finalizedPivotPositions {
+		values = append(values, input[pos])
+	}
+
+	sort.Ints(input2)
+
+	// if the finalizedPivotPositions is positioned corrected, it should remain in the same location
+	for i, pos := range finalizedPivotPositions {
+		if input2[pos] != values[i] {
+			t.Error("the pivot position not match", input2[pos], values[i])
+		}
+	}
+
+	log.Println(`TestRelocatePivots finished`)
+}
+
+func TestQsortWithBucket(t *testing.T) {
+	array := generateRandomSlice(1000000)
+	counters := sliceToCounters(array)
+
+	startTime := time.Now()
+	qsortWithBucket(array)
+
+	log.Println("TestQsortWithBucket elapsed time:", time.Since(startTime))
+	if isAscSorted(array) == false {
+		t.Error("the sorting is buggy, isAscSorted failed")
+	}
+	if verifySliceCounters(array, counters) == false {
+		t.Error("the sorting is buggy, verifySliceCounters failed")
+	}
+}
+
+func TestQsortWithBucketV3(t *testing.T) {
+	array := generateRandomSlice(1000000)
+	counters := sliceToCounters(array)
+
+	startTime := time.Now()
+	qsortWithBucketV3(array)
+
+	log.Println("TestQsortWithBucketV3 elapsed time:", time.Since(startTime))
+	if isAscSorted(array) == false {
+		t.Error("the sorting is buggy, isAscSorted failed")
+	}
+	if verifySliceCounters(array, counters) == false {
+		t.Error("the sorting is buggy, verifySliceCounters failed")
+	}
+
+}
+
+func TestQsortProd(t *testing.T) {
+	array := generateRandomSlice(1000000)
+	counters := sliceToCounters(array)
+
+	startTime := time.Now()
+	qsortProd(array)
+
+	log.Println("TestQsortProd elapsed time:", time.Since(startTime))
+	if isAscSorted(array) == false {
+		t.Error("the sorting is buggy, isAscSorted failed")
+	}
+	if verifySliceCounters(array, counters) == false {
+		t.Error("the sorting is buggy, verifySliceCounters failed")
+	}
+}
+
+func TestQsortProdV2(t *testing.T) {
+	array := generateRandomSlice(1000000)
+	counters := sliceToCounters(array)
+
+	startTime := time.Now()
+	qsortProdV2(array)
+
+	log.Println("TestQsortProdV2 elapsed time:", time.Since(startTime))
+	if isAscSorted(array) == false {
+		t.Error("the sorting is buggy, isAscSorted failed")
+	}
+	if verifySliceCounters(array, counters) == false {
+		t.Error("the sorting is buggy, verifySliceCounters failed")
+	}
+}
+
+func BenchmarkQsortProd(b *testing.B) {
+	array := generateRandomSlice(1000000)
+	qsortProd(array)
+}
+func BenchmarkQsortProdV2(b *testing.B) {
+	array := generateRandomSlice(1000000)
+	qsortProdV2(array)
+}
+
+func BenchmarkQsortStandard(b *testing.B) {
+	array := generateRandomSlice(1000000)
+	sort.Ints(array)
+}
