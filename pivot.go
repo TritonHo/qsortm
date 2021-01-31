d9a46143f5aeeca6b9146a2c8e053e017cf410fe
Triton Ho <5516113+TritonHo@users.noreply.github.com>
2021-01-31 22:34:45 +0000

    add getPivotPos

---
+++ pivot.go
@@ -0,0 +1,36 @@
+package qsortm
+
+func medianOfThree(data Interface, posA, posB, posC int) int {
+	ab := data.Less(posA, posB)
+	bc := data.Less(posB, posC)
+
+	// (a < b < c) or (c < b < a)
+	if (ab && bc) || (ab == false && bc == false) {
+		return posB
+	}
+
+	ac := data.Less(posA, posC)
+	// (b < a < c) or (c < a < b)
+	if (ab == false && ac) || (ac == false && ab) {
+		return posA
+	}
+
+	return posC
+}
+
+func getPivotPos(data Interface, startPos, endPos int) int {
+
+	m := (endPos + startPos) / 2
+	if endPos-startPos <= 40 {
+		return medianOfThree(data, startPos, m, endPos-1)
+	}
+
+	// copied from sort/sort.go
+	// Tukey's ``Ninther,'' median of three medians of three.
+	s := (endPos - startPos) / 8
+	p1 := medianOfThree(data, startPos, startPos+s, startPos+2*s)
+	p2 := medianOfThree(data, m, m-s, m+s)
+	p3 := medianOfThree(data, endPos-1, endPos-1-s, endPos-1-2*s)
+
+	return medianOfThree(data, p1, p2, p3)
+}
